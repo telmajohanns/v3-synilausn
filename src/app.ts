db7525efa73a7435d7f8bafab684d9e23fa3ec4d
telmajohanns <tej2@hi.is>
2023-04-02 20:45:50 +0000

    testa fyrir hysingu

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors);
 app.use(router);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
